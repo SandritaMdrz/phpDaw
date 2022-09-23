5e9161766e94da0b03da86f95b0aa4e7253cd757
SandritaMdrz <113590000+SandritaMdrz@users.noreply.github.com>
2022-09-23 10:43:28 +0200

    Update ej1s.php

--- ut1/ej1s.php
+++ ut1/ej1s.php
@@ -5,16 +5,18 @@
 
 		$ip="192.168.206.220";
 		
-		$p1=strpos ($ip,".",0);
+		$p1=strpos ($ip,".");
 		$p2=strpos ($ip,".",($p1+1));
-		$p3=strpos ($ip,".",($p2+1));
+		$p3=strrpos ($ip,".",($p2));
 		
-		printf ("La ip $ip en binario es: %08b.%08b.%08b.%08b<br>",
-		(substr($ip,0,$p1)),
-		(substr($ip,($p1)+1,($p2-$p1))),
-		(substr($ip,($p2)+1,($p3-$p2))),
-		(substr($ip,($p3)+1,($p3))));
-
+		
+		$punto1=substr($ip,0,$p1);
+		$punto2=substr($ip,($p1)+1,($p2-$p1));
+		$punto3=substr($ip,($p2)+1,($p3-$p2));
+		$punto4=substr($ip,$p3+1,$p3);
+		
+		
+		printf ("La ip $punto1.$punto2.$punto3.$punto4 en binario es:  %08b.%08b.%08b.%08b",$punto1,$punto2,$punto3,$punto4);
 ?>
 </BODY>
 </HTML>
