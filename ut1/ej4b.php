f14868ef19897cc3fcb658a0093adb30f2a1b814
SandritaMdrz <113590000+SandritaMdrz@users.noreply.github.com>
2022-09-26 10:45:18 +0200

    Create ej4b.php

---
+++ ut1/ej4b.php
@@ -0,0 +1,15 @@
+<HTML>
+<HEAD><TITLE> EJ4B – Tabla Multiplicar</TITLE></HEAD>
+<BODY>
+<?php
+$num="2";
+
+	
+	for($i=1;$i<=10;$i++){
+		$res=$num*$i;
+		echo "$num x $i = $res<br>";
+	}
+
+?>
+</BODY>
+</HTML>
