712e1071d52034bb43f43a634ce65f6ac9edaa17
SandritaMdrz <113590000+SandritaMdrz@users.noreply.github.com>
2022-09-28 09:57:17 +0200

    Add files via upload

---
+++ ut2_02/ej6b.php
@@ -0,0 +1,15 @@
+<HTML>
+<HEAD><TITLE> EJ6B – Factorial</TITLE></HEAD>
+<BODY>
+<?php
+$num="5";
+$fact=1;
+	for($i=$num;$i>1;$i--){
+		$fact=$fact*$i;
+		
+	}
+	
+	echo "el factorial de $num!  es $fact";
+?>
+</BODY>
+</HTML>
